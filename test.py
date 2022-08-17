8a9c2a59d3d13dd366f17e72a1219eaac043052f
awexander <shafiqyaacob11@gmail.com>
2022-08-17 17:36:33 +0800

    test.py

--- test.py
+++ test.py
@@ -1,18 +1,19 @@
 import getmatches
 import asyncio
+from datetime import datetime as datetime
 
 valorant = getmatches.getmatchinfo()
 
 async def main():
+    now = datetime.now()
     result , error= await valorant.getmatches('ap', 'awexander', '007')
     if result is True:
-        print(valorant.match.map)
-        print(valorant.match.gamemode)
-        print(valorant.match.roundWon)
-        print(valorant.match.roundLost)
+        print(f'{valorant.match.map}: {valorant.match.gamemode}')
+        print(f'{valorant.match.roundWon}:{valorant.match.roundLost}')
         print(int(round(valorant.match.adr)))
         print(int(round(valorant.match.headshot)))
     else:
         print(error)
-        
+
+    print(datetime.now() - now)
 asyncio.get_event_loop().run_until_complete(main())
