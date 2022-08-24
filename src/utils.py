122a7732b8a4a16fbd364cc4601913cd92298496
awexander <shafiqyaacob11@gmail.com>
2022-08-24 18:06:54 +0800

    get events and tasks via module

--- src/utils.py
+++ src/utils.py
@@ -1,62 +1,63 @@
-import asyncio
-from email import message
 import discord
-from discord.ext import commands
 from src.CONFIG import (
     ANNOUNCECHANNEL,
     LOGCHANNEL,
     GREEN, 
     RED,
     BLUE,
-    REPORTCHANNEL
+    REPORTCHANNEL,
+    SLASH
 )
 import os
 import json
 
-bot = commands.Bot
+class utils():
+    def __init__(self, bot) -> None:
+        self.bot = bot
+        pass
+    async def readprevdata(self):
+        try:
+            path = os.getcwd()
+            with open(path + f'{SLASH}data{SLASH}updates.json', 'r') as w:
+                data = json.loads(w.read())
+        except Exception as error:
+            await self.ERROR(f'failed to load updates file \n {error}')
 
-async def readprevdata():
-    try:
-        path = os.cwd()
-        with open(path +'/data/updates.json', 'r') as w:
-            data = json.loads(w.read())
-    except Exception as error:
-        await ERROR(f'failed to load updates file \n {error}')
+        try:
+            prevUpdate = data['updates']
+        except Exception as error:
+            await self.ERROR(f'failed to load previous updates details \n {error}')
 
-    try:
-        prevUpdate = data['updates']
-    except Exception as error:
-        await ERROR(f'failed to load previous updates details \n {error}')
+        try:
+            prevMaintenance = data['maintenances']
+        except Exception as error:
+            await self.ERROR(f'failed to load previous maintenances details \n {error}')
 
-    try:
-        prevMaintenance = data['maintenances']
-    except Exception as error:
-        await ERROR(f'failed to load previous maintenances details \n {error}')
+        try:
+            prevIncidents = data['incidents']
+        except Exception as error:
+            await self.ERROR(f'failed to load previous incidents details \n {error}')
+        
+        return prevUpdate, prevMaintenance, prevIncidents
 
-    try:
-        prevIncidents = data['incidents']
-    except Exception as error:
-        await ERROR(f'failed to load previous incidents details \n {error}')
-    
-    return prevUpdate, prevMaintenance, prevIncidents
+    async def ERROR(self, message):
+        embed = discord.Embed(title='[ERROR]', description=message, color=RED)
+        return await self.log(content=embed)
 
-async def ERROR(message):
-    embed = discord.Embed(title='[ERROR]', description=message, color=RED)
-    return await log(content=embed)
+    async def BOT(self, message):
+        embed = discord.Embed(title='[BOT]', description=message, color=GREEN)
+        return await self.log(content=embed)
 
-async def BOT(message):
-    embed = discord.Embed(title='[BOT]', description=message, color=GREEN)
-    return await log(content=embed)
+    async def SERVER(self, message):
+        embed = discord.Embed(title='[SERVER]', description=message, color=BLUE)
+        return await self.log(content=embed)
 
-async def SERVER(message):
-    embed = discord.Embed(title='[SERVER]', description=message, color=BLUE)
-    return await log(content=embed)
+    async def REPORT(self, embed=discord.Embed):
+        return await self.bot.get_channel(REPORTCHANNEL).send(embed=embed)
 
-async def REPORT(embed=discord.Embed):
-    return await bot.get_channel(REPORTCHANNEL).send(embed=embed)
+    async def NOTIFICATION(self, message=str):
+        return await self.bot.get_channel(ANNOUNCECHANNEL).send(message)
 
-async def NOTIFICATION(message=str):
-    return await bot.get_channel(ANNOUNCECHANNEL).send(message)
-    
-async def log(message=str, content=discord.Embed):
-    return await bot.get_channel(LOGCHANNEL).send(message=message, embed=content)
+    async def log(self, content=discord.Embed):
+        channel = self.bot.get_channel(LOGCHANNEL)
+        return await channel.send(embed=content)
