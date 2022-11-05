958b5fa2b8bd8924c46545cc1546a9043057a3cd
awexander <shafiqyaacob@gmail.com>
2022-11-05 15:43:06 +0800

    fix headshots and adr accuracy

--- src/matches.py
+++ src/matches.py
@@ -1,7 +1,7 @@
 
 import aiohttp
 import datetime as dt
-from src.CONFIG import NOMATCHPLAYED, SUCCESMATCH
+from src.CONFIG import NOMATCHPLAYED, SUCCESMATCH, FAILREQUEST
 
 class getmatchinfo():
     def __init__(self):
@@ -26,23 +26,27 @@ class getmatchinfo():
         self._tag = tag
         self.matches = await self._request(name, tag)
         try:
+            if self.matches.get('status') == 201:
+                return NOMATCHPLAYED
+                
             self.matchlist.matchid = await self._getMatchID()
-            self.matchlist.map = await self._getmap()
-            self.matchlist.gamemode = await self._getGameMode()
-            self.matchlist.matchdate = await self._getmatchdate()
-
-            self.matchstats = await self._getstats()
-            self.matchlist.agent = await self._getAgent()
-            self.matchlist.rank = await self._getRank()
-            self.matchlist.roundWon, self.matchlist.roundLost = await self._getMatchResult()
-            self.matchlist.headshot = await self._getHeadshot()
-            self.matchlist.kda = await self._getkda()
-            self.matchlist.adr = await self._getadr()
             
             return SUCCESMATCH
         except:
-            return self.matches
+            return FAILREQUEST
 
+    async def processmatch(self):
+        self.matchlist.map = await self._getmap()
+        self.matchlist.gamemode = await self._getGameMode()
+        self.matchlist.matchdate = await self._getmatchdate()
+
+        self.matchstats = await self._getstats()
+        self.matchlist.agent = await self._getAgent()
+        self.matchlist.rank = await self._getRank()
+        self.matchlist.roundWon, self.matchlist.roundLost = await self._getMatchResult()
+        self.matchlist.headshot = await self._getHeadshot(self.matchstats)
+        self.matchlist.kda = await self._getkda(self.matchstats)
+        self.matchlist.adr = await self._getadr(self.matchstats)
 
     async def _request(self, name, tag):
         url = f'https://api.henrikdev.xyz/valorant/v3/matches/{self.region}/{name}/{tag}'
@@ -53,8 +57,8 @@ class getmatchinfo():
                     for data in matches['data']:
                         if data['metadata']['mode'] == 'Competitive':
                             return data
-                        else:
-                            return NOMATCHPLAYED
+                        
+                    return NOMATCHPLAYED
                 else:
                     return matches
 
@@ -70,10 +74,10 @@ class getmatchinfo():
                 "team": player['team'],
                 "rank": player['currenttier_patched'],
                 "agent": player['character'],
-                "acs": await self._getacs(),
-                "headshot": await self._getHeadshot(),
-                "kda": await self._getkda(),
-                "adr": await self._getadr()
+                "acs": await self._getacs(player),
+                "headshot": await self._getHeadshot(player),
+                "kda": await self._getkda(player),
+                "adr": await self._getadr(player)
             }
             data.append(playerData)
         match = {
@@ -132,18 +136,40 @@ class getmatchinfo():
     async def _getTeamResult(self, team):
         return self.matches['teams'][team.lower()]['rounds_won']
     
-    async def _getacs(self):
+    async def _getacs(self, player):
         return int(round(self.matchstats['stats']['score'] / self.matches['metadata']['rounds_played']))
 
-    async def _getHeadshot(self):
-        return int(round((self.matchstats['stats']['headshots'] / (self.matchstats['stats']['headshots'] + self.matchstats['stats']['bodyshots'] + self.matchstats['stats']['legshots'])) * 100))
+    async def _getHeadshot(self, player):
+        headshots = 0
+        totalshots = 0
 
-    async def _getkda(self):
+        for rounds in self.matches.get('rounds'):
+            for plyr in rounds.get('player_stats'):
+                if plyr.get('player_puuid') == player.get('puuid'):
+                    for dmg_event in plyr.get('damage_events'):
+                        if dmg_event.get('receiver_puuid') != player.get('puuid'):
+                            headshots += dmg_event.get('headshots')
+                            totalshots += (dmg_event.get('headshots') +
+                                           dmg_event.get('bodyshots') + dmg_event.get('legshots'))
+                    break
+
+        return round((headshots / totalshots) * 100)
+
+    async def _getkda(self, player):
         return [self.matchstats['stats']['kills'], self.matchstats['stats']['deaths'], self.matchstats['stats']['assists'], float(round(self.matchstats['stats']['kills'] / self.matchstats['stats']['deaths'],1))]
     
-    async def _getadr(self):
-        return int(round(self.matchstats['damage_made'] / self.matches['metadata']['rounds_played']))
-    
+    async def _getadr(self, player):
+        damage_made = 0
+        for rounds in self.matches.get('rounds'):
+            for plyr in rounds.get('player_stats'):
+                if plyr.get('player_puuid') == player.get('puuid'):
+                    for dmg_event in plyr.get('damage_events'):
+                        if dmg_event.get('receiver_puuid') != player.get('puuid'):
+                            damage_made += dmg_event.get('damage')
+                    break
+
+        return round(damage_made / self.matches.get('metadata').get('rounds_played'))
+        
     class _latestmatch():
         def __init__(self, puuid, matchid, map, mode, matchdate, agent, rank, roundWon, roundLost, headshot, kda, adr):
             self.puuid = puuid
@@ -158,4 +184,3 @@ class getmatchinfo():
             self.headshot = headshot
             self.kda = kda
             self.adr = adr
-
